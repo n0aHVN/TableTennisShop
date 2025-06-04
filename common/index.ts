6c106204775a7680709378bb3adad909ec0774ae
n0aHVN <superherodung123@gmail.com>
2025-06-04 22:36:31 +0700

    Update index.ts

--- common/index.ts
+++ common/index.ts
@@ -8,7 +8,7 @@ export * from './middlewares/error-handler';
 export * from './middlewares/validate-request-middleware';
 export * from './middlewares/current-user-middleware';
 
-export * from './models/client.model';
+export * from './models/user.model';
 export * from './models/product.model';
 export * from './models/racket.model';
 export * from './models/shirt.model';
