83796bb2262f2671fa2e2aa97948ac14e4a0f76e
n0aHVN <superherodung123@gmail.com>
2025-06-05 07:25:20 +0700

    Update address.schema.ts

--- common/models/address.schema.ts
+++ common/models/address.schema.ts
@@ -4,6 +4,7 @@ export interface IAddress {
   province: string;
   district: string;
   ward: string;
+  address: string;
   phone_number: string;
 }
 
@@ -12,6 +13,7 @@ export const AddressSchema = new Schema<IAddress>(
     province: { type: String, required: true },
     district: { type: String, required: true },
     ward: { type: String, required: true },
+    address: {type: String, required: true},
     phone_number: { type: String, required: true },
   },
   { _id: false }//This schema don't need Address Schema
