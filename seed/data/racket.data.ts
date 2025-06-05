d58a356a2ce45bec2729cb2a77f0ea081f6ff6f5
n0aHVN <superherodung123@gmail.com>
2025-06-05 07:26:07 +0700

    Update Initial Data

---
+++ seed/data/racket.data.ts
@@ -0,0 +1,99 @@
+import { ProductEnum, RacketAttrs } from "@tabletennisshop/common";
+
+export const clientData: RacketAttrs[] = [
+    {
+        type: ProductEnum.Racket,
+        sport: "Table Tennis",
+        name: "Zhang Jike ALC",
+        brand: "Butterfly",
+        description:"This is the description",
+        attributes:{
+            speed: "11.8",
+            vibration: "10.3",
+            weight: "82gr",
+            composition: "5 Wood Layers + 2 Arylate Carbon Layers",
+            size: "157x150mm",
+            thickness: "5.8mm"
+        },
+        price: 8000000
+    },
+    {
+        type: ProductEnum.Racket,
+        sport: "Table Tennis",
+        name: "Viscaria ALC",
+        brand: "Butterfly",
+        description:"This is the description",
+        attributes:{
+            speed: "11.8",
+            vibration: "10.3",
+            weight: "82gr",
+            composition: "5 Wood Layers + 2 Arylate Carbon Layers",
+            size: "157x150mm",
+            thickness: "5.8mm"
+        },
+        price: 3200000
+    },
+    {
+        type: ProductEnum.Racket,
+        sport: "Table Tennis",
+        name: "Amultart ZLC",
+        brand: "Butterfly",
+        description:"This is the description",
+        attributes:{
+            speed: "12.2",
+            vibration: "13.7",
+            weight: "76gr",
+            composition: "3 Wood Layers + 2 Arylate Carbon Layers",
+            size: "157x150mm",
+            thickness: "7.1mm"
+        },
+        price: 3800000
+    },
+    {
+        type: ProductEnum.Racket,
+        sport: "Table Tennis",
+        name: "Fan Zhendong ALC",
+        brand: "Butterfly",
+        description:"This is the description",
+        attributes:{
+            speed: "11.8",
+            vibration: "10.3",
+            composition: "5 Wood Layers + 2 Arylate Carbon Layers",
+            size: "157x150mm",
+            thickness: "5.8mm"
+        },
+        price: 3000000
+    },
+        {
+        type: ProductEnum.Racket,
+        sport: "Table Tennis",
+        name: "Zhang Jike ALC NDN",
+        brand: "Butterfly",
+        description:"This is the description",
+        attributes:{
+            speed: "11.8",
+            vibration: "10.3",
+            weight: "82gr",
+            composition: "5 Wood Layers + 2 Arylate Carbon Layers",
+            size: "157x150mm",
+            thickness: "5.8mm"
+        },
+        price: 9000000
+    },
+        {
+        type: ProductEnum.Racket,
+        sport: "Table Tennis",
+        name: "Timo Boll ALC",
+        brand: "Butterfly",
+        description:"This is the description",
+        attributes:{
+            speed: "11.8",
+            vibration: "10.3",
+            weight: "85gr",
+            composition: "5 Wood Layers + 2 Carbon Layers",
+            size: "157x150mm",
+            thickness: "5.8mm"
+        },
+        price: 3000000
+    }
+];
---
+++ seed/data/user.data.ts
@@ -0,0 +1,175 @@
+import { UserAttrs } from "@tabletennisshop/common";
+import { UserStatusEnum } from "@tabletennisshop/common/build/enums/user-status.enum";
+
+export const userData: UserAttrs[] = [
+  {
+    username: "superherodung123",
+    email: "superherodung123@gmail.com",
+    password: "123",
+    full_name: "Nguyen Tri Dung",
+    type: "Client",
+    status: UserStatusEnum.Enalble,
+    addresses: [
+      {
+        province: "Ho Chi Minh City",
+        district: "Thu Duc City",
+        ward: "Truong Tho",
+        address: "123 Truong Tho Street",
+        phone_number: "0382388607",
+      }
+    ]
+  },
+  {
+    username: "tranquang456",
+    email: "tranquang456@gmail.com",
+    password: "123",
+    full_name: "Tran Quang",
+    type: "Client",
+    status: UserStatusEnum.Enalble,
+    addresses: [
+      {
+        province: "Ha Noi",
+        district: "Ba Dinh",
+        ward: "Lien Hoa",
+        address: "456 Hoa Binh Road",
+        phone_number: "0987654321",
+      }
+    ]
+  },
+  {
+    username: "lethu987",
+    email: "lethu987@gmail.com",
+    password: "123",
+    full_name: "Le Thi Thu",
+    type: "Client",
+    status: UserStatusEnum.Enalble,
+    addresses: [
+      {
+        province: "Da Nang",
+        district: "Hai Chau",
+        ward: "Hoa Thuan",
+        address: "789 Bien Hoa Street",
+        phone_number: "0911223344",
+      }
+    ]
+  },
+  {
+    username: "minhtam321",
+    email: "minhtam321@gmail.com",
+    password: "123",
+    full_name: "Pham Minh Tam",
+    type: "Client",
+    status: UserStatusEnum.Enalble,
+    addresses: [
+      {
+        province: "Can Tho",
+        district: "Ninh Kieu",
+        ward: "Tan An",
+        address: "12 Le Loi Avenue",
+        phone_number: "0909332211",
+      }
+    ]
+  },
+  {
+    username: "hoangnam456",
+    email: "hoangnam456@gmail.com",
+    password: "123",
+    full_name: "Hoang Nam",
+    type: "Client",
+    status: UserStatusEnum.Enalble,
+    addresses: [
+      {
+        province: "Hue",
+        district: "Phu Hoi",
+        ward: "An Cuu",
+        address: "34 An Duong Vuong",
+        phone_number: "0911888777",
+      }
+    ]
+  },
+  {
+    username: "bichngoc789",
+    email: "bichngoc789@gmail.com",
+    password: "123",
+    full_name: "Nguyen Bich Ngoc",
+    type: "Client",
+    status: UserStatusEnum.Enalble,
+    addresses: [
+      {
+        province: "Nha Trang",
+        district: "Vinh Hai",
+        ward: "Vinh Tho",
+        address: "67 Tran Phu Street",
+        phone_number: "0933445566",
+      }
+    ]
+  },
+  {
+    username: "ducthang112",
+    email: "ducthang112@gmail.com",
+    password: "123",
+    full_name: "Le Duc Thang",
+    type: "Client",
+    status: UserStatusEnum.Enalble,
+    addresses: [
+      {
+        province: "Hai Phong",
+        district: "Le Chan",
+        ward: "Niem Nghia",
+        address: "88 Nguyen Van Linh",
+        phone_number: "0977223344",
+      }
+    ]
+  },
+  {
+    username: "huyenanh2000",
+    email: "huyenanh2000@gmail.com",
+    password: "123",
+    full_name: "Do Huyen Anh",
+    type: "Client",
+    status: UserStatusEnum.Enalble,
+    addresses: [
+      {
+        province: "Vung Tau",
+        district: "Thang Tam",
+        ward: "Ngoc Hien",
+        address: "101 Vo Thi Sau",
+        phone_number: "0955112233",
+      }
+    ]
+  },
+  {
+    username: "manhkhoa654",
+    email: "manhkhoa654@gmail.com",
+    password: "123",
+    full_name: "Nguyen Manh Khoa",
+    type: "Client",
+    status: UserStatusEnum.Enalble,
+    addresses: [
+      {
+        province: "Dak Lak",
+        district: "Buon Ma Thuot",
+        ward: "Ea Tam",
+        address: "145 Hoang Dieu",
+        phone_number: "0944221100",
+      }
+    ]
+  },
+  {
+    username: "thanhloan888",
+    email: "thanhloan888@gmail.com",
+    password: "123",
+    full_name: "Pham Thanh Loan",
+    type: "Client",
+    status: UserStatusEnum.Enalble,
+    addresses: [
+      {
+        province: "Quang Ninh",
+        district: "Ha Long",
+        ward: "Bai Chay",
+        address: "55 Bai Chay Street",
+        phone_number: "0966554433",
+      }
+    ]
+  }
+];
