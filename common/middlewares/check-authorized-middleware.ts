cc27ff6213eea1aa939f1292b1ab70ac352efdc4
n0aHVN <superherodung123@gmail.com>
2025-07-10 22:07:17 +0700

    Update check-authorized-middleware.ts

--- common/middlewares/check-authorized-middleware.ts
+++ common/middlewares/check-authorized-middleware.ts
@@ -1,9 +1,9 @@
-import { NextFunction } from "express"
-import axios from "axios";
+import { NextFunction, Request, Response } from "express"
 import { NotAuthorizedError } from "../errors/not-authorized-error";
+
 export const CheckAuthorizedMiddleware= async (req: Request, res: Response, next: NextFunction)=>{
-    const axiosRes = await axios.get("localhost:3000/api/users/current-user");
-    if (axiosRes.status != 200){
+    //This can be wrong
+    if (!req.session?.jwt){
         throw new NotAuthorizedError("Not Authorized");
     }
     next();
