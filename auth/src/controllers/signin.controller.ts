f0478ced4e975ad8679e1c0de3977ed61730ca46
n0aHVN <superherodung123@gmail.com>
2025-07-10 22:06:51 +0700

    Create signin.controller.ts

---
+++ auth/src/controllers/signin.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import { body } from "express-validator";
+import { UserService } from "../services/user.service";
+
+export const signinValidationRules = [
+  body("email")
+    .notEmpty().withMessage("Email is required"),
+  body("password")
+    .trim()
+    .notEmpty().withMessage("Password is required")
+    .isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters"),
+];
+
+export const signinController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+    const { email, password } = req.body;
+    const { clientJwt, message } = await UserService.authenticateUser({ email, password });
+    req.session = { jwt: clientJwt };
+    res.status(200).send(message);
+};
+
