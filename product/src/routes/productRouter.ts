2d06b32d1d3ed50a1872061c0654f47cb25d8dc7
n0aHVN <superherodung123@gmail.com>
2025-07-14 13:32:50 +0700

    Update productRouter.ts

--- product/src/routes/productRouter.ts
+++ product/src/routes/productRouter.ts
@@ -1,15 +1,15 @@
-import { ProductDoc, ProductModel } from "@tabletennisshop/common";
 import express, { Request, Response, Router } from "express";
 import { ProductController } from "../controller/product.controller";
 import { IPaginationMetaData} from "../types/pagination.types";
 import { createPaginationMeta } from "../utils/pagination";
+import { ProductEnum } from "@tabletennisshop/common/build/enums/product.enum";
 
 const productRouter = express.Router();
 
 productRouter.get("/api/products/",async (req: Request, res: Response)=>{
     const page = parseInt(req.query.page as string) || 1;
-
-    const products = await ProductController.pagingAllProducts({page});
+    const type = req.query.type as ProductEnum;
+    const products = await ProductController.pagingProducts({page, type});
     const totalItems = await ProductController.countAllProducts();
 
     const meta : IPaginationMetaData = createPaginationMeta({
