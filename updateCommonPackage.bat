d7f6ec95b3bf4151fec6bfbc045a37dec8bc37e9
n0aHVN <superherodung123@gmail.com>
2025-07-14 13:34:26 +0700

    Create updateCommonPackage.bat

---
+++ updateCommonPackage.bat
@@ -0,0 +1,8 @@
+:: Run npm update in 'auth' in a new window
+start cmd /c "cd auth && npm update @tabletennisshop/common && pause"
+
+:: Run npm update in 'order' in a new window
+start cmd /c "cd order && npm update @tabletennisshop/common && pause"
+
+:: Run npm update in 'product' in a new window
+start cmd /c "cd product && npm update @tabletennisshop/common && pause"
