dbf371708b97d4807413da9b7b3fdc202dcf3197
n0aHVN <superherodung123@gmail.com>
2025-08-20 05:26:38 +0700

    Update product.service.ts

--- product/src/services/product.service.ts
+++ product/src/services/product.service.ts
@@ -1,16 +1,8 @@
-import { ProductDoc, ProductEnum, ProductModel } from "@tabletennisshop/common";
+import { ProductDoc, ProductModel } from "@tabletennisshop/common";
 
 export class ProductService{
     static async getProductBaseOnId({id}:{id: number}): Promise<ProductDoc|null>{
         return await ProductModel.findById(id);
-    }
-    static async pagingProducts({page, type}: {page: number, type?: ProductEnum}): Promise<ProductDoc[]|null>{
-        const limit = 24;
-        const skip = (page - 1) * limit;
-        const products = type ? await ProductModel.find().where('type').equals(type)
-                            .select(type).skip(skip).limit(limit).exec()
-                            : await ProductModel.find().skip(skip).limit(limit).exec();
-        return products;
     }
 
 }
