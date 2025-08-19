519e2f7f2f01baed13efa5156d3a951a55f86b12
n0aHVN <superherodung123@gmail.com>
2025-08-20 05:26:22 +0700

    Update product.controller.ts

--- product/src/controller/product.controller.ts
+++ product/src/controller/product.controller.ts
@@ -1,12 +1,6 @@
-import {ProductDoc, ProductEnum, ProductModel} from '@tabletennisshop/common';
+import {ProductDoc, ProductModel} from '@tabletennisshop/common';
 import { ProductService } from '../services/product.service';
 export class ProductController{
-    static async pagingProducts(
-        {type, page}:{type: ProductEnum, page: number}
-    ): Promise<ProductDoc[]|null>{
-        const products = await ProductService.pagingProducts({page, type});
-        return products;
-    }
     static async countAllProducts(): Promise<number>{
         const result = await ProductModel.countDocuments().exec();
         return result;
