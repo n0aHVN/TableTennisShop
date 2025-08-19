9595ca062b5e6efa47801d0a7671593404de8e12
n0aHVN <superherodung123@gmail.com>
2025-08-19 18:55:15 +0700

    Update updateCommonPackage.bat

--- updateCommonPackage.bat
+++ updateCommonPackage.bat
@@ -1,8 +1,10 @@
 :: Run npm update in 'auth' in a new window
-start cmd /c "cd auth && npm update @tabletennisshop/common && pause"
+start cmd /c "cd auth && npm install @tabletennisshop/common@1.0.53 && pause"
 
 :: Run npm update in 'order' in a new window
-start cmd /c "cd order && npm update @tabletennisshop/common && pause"
+start cmd /c "cd order && npm install @tabletennisshop/common@1.0.53 && pause"
 
 :: Run npm update in 'product' in a new window
-start cmd /c "cd product && npm update @tabletennisshop/common && pause"
+start cmd /c "cd product && npm install @tabletennisshop/common@1.0.53 && pause"
+
+start cmd /c "cd seed && npm install @tabletennisshop/common@1.0.53 && pause"
