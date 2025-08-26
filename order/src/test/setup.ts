cfba66d4fae23d552b5e3ae2322323c7d92c49a2
n0aHVN <superherodung123@gmail.com>
2025-08-26 13:43:44 +0700

    Create setup.ts

---
+++ order/src/test/setup.ts
@@ -0,0 +1,76 @@
+import request from 'supertest';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose, {Types} from 'mongoose';
+import { OrderAttrs, OrderDoc, OrderModel } from '../models/order.model';
+import {OrderStatusEnum, PaymentMethodEnum } from '@tabletennisshop/common';
+import { InventoryAttrs, InventoryDoc, InventoryModel } from '../models/inventory.model';
+import jwt from 'jsonwebtoken';
+declare global{
+    function signin(): Promise<string>;
+    function prepareData(): Promise<{ inventoryDoc: InventoryDoc; orderDoc: OrderDoc }>;
+}
+
+let mongo:any;
+let userId: Types.ObjectId = new Types.ObjectId();
+beforeAll(
+    async () => {
+        process.env.JWT_KEY = "secretkey";
+        mongo = await MongoMemoryServer.create();
+        const mongoUrl = mongo.getUri();
+        await mongoose.connect(mongoUrl);
+    }
+)
+
+beforeEach(
+    async ()=>{
+        jest.clearAllMocks();
+        const collections = await mongoose.connection.db!.collections();
+        for (const collection of collections) {
+            await collection.deleteMany({});
+        }
+    }
+)
+afterAll(
+    async()=>{
+        await mongo.stop();
+        await mongoose.connection.close();
+    }
+)
+
+global.signin = async ()=>{
+    const email = 'test@test.com';
+    const payload = { _id: userId.toHexString(), email };
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
+    const sessionJson = JSON.stringify({ jwt: token });
+
+    const base64 = Buffer.from(sessionJson).toString('base64');
+    return `session=${base64}`;
+}
+
+global.prepareData = async()=>{
+    console.log("User ID:", userId);
+    const product_id = new mongoose.Types.ObjectId();
+    const inventory:InventoryAttrs = {
+        product_id: product_id,
+        total_quantity: 100
+    }
+    const inventoryDoc = InventoryModel.build(inventory);
+    await inventoryDoc.save();
+
+    const order : OrderAttrs = {
+        user_id: userId,
+        status: OrderStatusEnum.PENDING,
+        products: [{
+            product_id: product_id,
+            price: 100,
+            quantity: 2
+        }],
+        payment_method: PaymentMethodEnum.COD,
+
+    }
+    const orderDoc = OrderModel.build(order);
+    await orderDoc.save();
+
+
+    return { inventoryDoc, orderDoc };
+}
