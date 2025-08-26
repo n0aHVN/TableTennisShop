5bb2e81c9db9b0cb4d930bb560308db2e6889299
n0aHVN <superherodung123@gmail.com>
2025-08-26 13:43:41 +0700

    Update order-route.ts

--- order/src/routes/order-route.ts
+++ order/src/routes/order-route.ts
@@ -1,28 +1,26 @@
 import express, { Request, Response } from "express";
 import { OrderService } from "../services/order-service";
-import { ApiResponse, CurrentUserMiddleware, OrderDoc, OrderStatusEnum } from "@tabletennisshop/common";
+import { ApiResponse, CurrentUserMiddleware, OrderStatusEnum } from "@tabletennisshop/common";
 import { OrderCancelledPublisher } from "../events/publishers/OrderCancelledPublisher";
-import { SubjectsEnum } from "@tabletennisshop/common/build/enums/event-subject.enum";
 import { natsWrapper } from "../NatsWrapper";
+import { OrderDoc } from "../models/order.model";
 const router = express.Router();
 
-router.get("/api/orders",CurrentUserMiddleware, async (req: Request, res: Response<ApiResponse<OrderDoc[]|null>>) => {
+router.get("/api/orders",CurrentUserMiddleware, async (req: Request, res: Response<ApiResponse<OrderDoc[]>>) => {
     const userId = req.currentUser?._id as string;
     const orders = await OrderService.getOrdersByUserId(userId);
-    res.status(200).send({
+    const response : ApiResponse<OrderDoc[]> = {
         success: true,
         statusCode: 200,
         data: orders
-    });
+    }
+    res.status(200).send(response);
 });
 
-router.get("/api/orders/:id",CurrentUserMiddleware, async (req: Request, res: Response<ApiResponse<OrderDoc | null>>) => {
+router.get("/api/orders/:id",CurrentUserMiddleware, async (req: Request, res: Response<ApiResponse<OrderDoc>>) => {
     const orderId = req.params.id;
-    const userId = req.currentUser?._id as string;
-    const order = await OrderService.getOrderByUserId({ user_id: userId, order_id: orderId });
-    if (!order) {
-        throw new Error("Order not found");
-    }
+    const userId = req.currentUser?._id!;
+    const order = await OrderService.getOrderById({ userId, order_id: orderId });
     res.status(200).send({
         success: true,
         statusCode: 200,
@@ -30,19 +28,22 @@ router.get("/api/orders/:id",CurrentUserMiddleware, async (req: Request, res: Re
     });
 });
 
-router.patch("/api/orders/:orderId/status", CurrentUserMiddleware, async (req: Request, res: Response<ApiResponse<OrderDoc | null>>) => {
+router.patch("/api/orders/:orderId/status", CurrentUserMiddleware, async (req: Request, res: Response<ApiResponse<OrderDoc>>) => {
     const orderId = req.params.orderId;
-    const userId = req.currentUser?._id as string;
     const { status } = req.body;
-    
-    const order = await OrderService.getOrderByUserId({ user_id: userId, order_id: orderId });
+    const userId = req.currentUser?._id as string;
+
+    const order = await OrderService.getOrderById({ userId, order_id: orderId });
     if (!order) {
         throw new Error("Order not found");
     }
 
     order.status = status as OrderStatusEnum;
     if (status == "cancelled"){
-        new OrderCancelledPublisher(natsWrapper.client).publish(order);
+        new OrderCancelledPublisher(natsWrapper.client).publish({
+            id: order._id,
+            version: order.version
+        });
     }
     res.status(200).send({
         success: true,
