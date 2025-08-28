2f0c1df17bb02798f0bd5bc700ee28e2390b656d
n0aHVN <superherodung123@gmail.com>
2025-08-28 21:57:29 +0700

    Create ProductEventInterface.ts

---
+++ common/events/ProductEventInterface.ts
@@ -0,0 +1,25 @@
+import { Types } from "mongoose";
+import { SubjectsEnum } from "../enums/event-subject.enum";
+import { ProductStatusEnum } from "../enums/product-status.enum";
+
+interface ProductAttrs{
+  _id: string,
+  status: ProductStatusEnum,
+  price: number,
+  version: number
+}
+
+export interface ProductUpdatedEventInterface{
+    subject: SubjectsEnum.ProductUpdated;
+    data: ProductAttrs;
+}
+
+export interface ProductCreatedEventInterface{
+    subject: SubjectsEnum.ProductCreated;
+    data: ProductAttrs;
+}
+
+export interface ProductDeletedEventInterface{
+    subject: SubjectsEnum.ProductDeleted;
+    data: {_id: string};
+}
