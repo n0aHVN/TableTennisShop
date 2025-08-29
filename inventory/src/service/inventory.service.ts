d4cddf0ca65e250812f80d2c8ba76a26dbf1d2e3
n0aHVN <superherodung123@gmail.com>
2025-08-29 20:55:18 +0700

    Create inventory.service.ts

---
+++ inventory/src/service/inventory.service.ts
@@ -0,0 +1,91 @@
+import { BadRequestError, NotFoundError } from "@tabletennisshop/common";
+import { InventoryDoc, InventoryModel } from "../models/inventory.model";
+import { InventoryUpdatedPublisher } from "../events/publisher/InventoryUpdatedPublisher";
+import { natsWrapper } from "../NatsWrapper";
+import { Types } from "mongoose";
+import { InventoryCreatedPublisher } from "../events/publisher/InventoryCreatedPublisher";
+
+type UpdateInventoryParams = Omit<Partial<InventoryDoc>, "_id" | "version"> & {
+    _id: string;
+}
+
+export class InventoryService {
+
+    static async getInventoryIdByProductId(product_id: string) {
+        const inventory = await InventoryModel.findOne({ product_id });
+        if (!inventory) {
+            throw new NotFoundError("Inventory not found");
+        }
+        return inventory._id.toHexString();
+    }
+
+    static async addQuantity({ quantity, inventory_id }: { quantity: number, inventory_id: string }) {
+        const product = await InventoryModel.findById(inventory_id);
+        if (!product) {
+            throw new NotFoundError("Product not found");
+        }
+
+        product.total_quantity += quantity;
+        await product.save();
+        new InventoryUpdatedPublisher(natsWrapper.client).publish({
+            _id: product._id.toHexString(),
+            total_quantity: product.total_quantity,
+            version: product.version,
+            product_id: product.product_id.toHexString()
+        });
+        return product;
+    }
+
+    static async subtractQuantity({ quantity, inventory_id }: { quantity: number, inventory_id: string }) {
+        const product = await InventoryModel.findById(inventory_id);
+        if (!product) {
+            throw new NotFoundError("Product not found");
+        }
+        
+        product.total_quantity -= quantity;
+        if (product.total_quantity < 0) {
+            throw new BadRequestError("Insufficient quantity");
+        }
+        await product.save();
+        
+        new InventoryUpdatedPublisher(natsWrapper.client).publish({
+            _id: product._id.toHexString(),
+            total_quantity: product.total_quantity,
+            version: product.version,
+            product_id: product.product_id.toHexString()
+        });
+        return product;
+    }
+
+    static async addInventory({ product_id, quantity }: { product_id: string; quantity: number }) {
+        const inventory = await InventoryModel.build({
+            product_id,
+            total_quantity: quantity
+        });
+        await inventory.save();
+
+        new InventoryCreatedPublisher(natsWrapper.client).publish({
+            _id: inventory._id.toHexString(),
+            product_id: inventory.product_id.toHexString(),
+            total_quantity: inventory.total_quantity,
+            version: inventory.version
+        });
+
+        return inventory;
+    }
+    static async updateInventory(data: UpdateInventoryParams) {
+        const inventory = await InventoryModel.findOne({_id: data._id});
+        if(!inventory) throw new Error("Inventory not found");
+        if (data.product_id !== undefined) inventory.product_id = data.product_id;
+        if (data.total_quantity !== undefined) inventory.total_quantity = data.total_quantity;
+        inventory.set(data);
+        const updatedInventory = await inventory.save();
+        new InventoryUpdatedPublisher(natsWrapper.client).publish({
+            _id: updatedInventory._id.toHexString(),
+            total_quantity: updatedInventory.total_quantity,
+            version: updatedInventory.version,
+            product_id: updatedInventory.product_id.toHexString()
+        });
+        return updatedInventory;
+    }
+}
