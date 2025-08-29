1e15fff521c2f3a6b1a213cee42401e4e6c7dac1
n0aHVN <superherodung123@gmail.com>
2025-08-29 20:55:12 +0700

    Update route.ts

--- inventory/src/routes/route.ts
+++ inventory/src/routes/route.ts
@@ -1,12 +1,14 @@
 
 import express from 'express'
 import { addQuantityController } from '../controller/addItem.controller';
-import { subjectQuantityController } from '../controller/subjectItem.controller';
+import { subtractQuantityController } from '../controller/subjectItem.controller';
+import { updateInventoryController } from '../controller/updateInventory.controller';
 const router = express.Router();
 
 router.patch("/api/inventory/:id/add", addQuantityController);
-router.patch("/api/inventory/:id/subject", subjectQuantityController);
+router.patch("/api/inventory/:id/subtract", subtractQuantityController);
 
+router.put("/api/inventory/:id", updateInventoryController);
 
 
 export {router as inventoryRouter};
