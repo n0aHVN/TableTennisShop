98b19c7c5a9084f43a882c7ef85cc44e7cfeb061
n0aHVN <superherodung123@gmail.com>
2025-08-29 20:55:06 +0700

    Update subjectQuantity.test.ts

--- inventory/src/routes/__test__/subjectQuantity.test.ts
+++ inventory/src/routes/__test__/subjectQuantity.test.ts
@@ -3,12 +3,12 @@ import {app} from '../../app';
 import { natsWrapper } from '../../NatsWrapper';
 
 it("add quantity inventory", async () => {
-  const id = await global.addInventory();
+  const inventory = await global.addInventory();
   const response = await request(app)
-    .patch(`/api/inventory/${id}/subject`)
+    .patch(`/api/inventory/${inventory._id.toHexString()}/subtract`)
     .set("Cookie", await global.signin())
     .send({
-      id: id,
+      product_id: inventory.product_id.toHexString(),
       quantity: 10,
     });
 
