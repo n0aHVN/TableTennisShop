6774db34d3ad74f8fc7fbcfee0cbce240afa64f3
n0aHVN <superherodung123@gmail.com>
2025-08-29 20:54:24 +0700

    Update OrderEventInterface.ts

--- common/events/OrderEventInterface.ts
+++ common/events/OrderEventInterface.ts
@@ -4,21 +4,26 @@ import { OrderStatusEnum } from "../enums/order-status.enum";
 import { PaymentMethodEnum } from "../enums/payment-method.enum";
 
 interface OrderAttrs {
-  _id: string;
-  user_id: string;// FK to User
-  status: OrderStatusEnum;
-  payment_method: PaymentMethodEnum;
-  total_price: number;
-  version: number;
+    _id: string;
+    user_id: string;// FK to User
+    products: {
+        product_id: string;
+        price: number;
+        quantity: number;
+    }[];
+    status: OrderStatusEnum;
+    payment_method: PaymentMethodEnum;
+    total_price: number;
+    version: number;
 }
 
 
-export interface OrderCreatedEventInterface{
+export interface OrderCreatedEventInterface {
     subject: SubjectsEnum.OrderCreated;
     data: OrderAttrs;
 }
 
-export interface OrderUpdatedEventInterface{
+export interface OrderUpdatedEventInterface {
     subject: SubjectsEnum.OrderUpdated;
     data: OrderAttrs;
 }
@@ -26,7 +31,7 @@ export interface OrderUpdatedEventInterface{
 export interface OrderCancelledEventInterface {
     subject: SubjectsEnum.OrderCancelled;
     data: {
-        id: string;
+        _id: string;
         version: number;
     };
 }
