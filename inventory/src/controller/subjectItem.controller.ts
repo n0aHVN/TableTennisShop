dea2893c8111717e636cc96fd408047e66f0cc28
n0aHVN <superherodung123@gmail.com>
2025-08-29 20:54:37 +0700

    Update subjectItem.controller.ts

--- inventory/src/controller/subjectItem.controller.ts
+++ inventory/src/controller/subjectItem.controller.ts
@@ -3,15 +3,15 @@ import { body, param } from "express-validator";
 import { ApiResponse } from "@tabletennisshop/common/build/types/base";
 import { InventoryService } from "../service/inventory.service";
 
-export const subjectQuantityController =  [
+export const subtractQuantityController =  [
     param("id").isMongoId().withMessage("Invalid product ID"),
     body("quantity").isInt({min:1}).withMessage("Invalid quantity"),
     async (req: Request, res: Response)=>{
-        const {id: product_id} = req.params;
+        const {id: inventory_id} = req.params;
         const {quantity} = req.body;
 
         // Add item to inventory
-        const result = await InventoryService.subjectQuantity({product_id, quantity});
+        const result = await InventoryService.subtractQuantity({inventory_id, quantity});
 
         const response: ApiResponse = {
             statusCode: 200, 
