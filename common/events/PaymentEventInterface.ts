95cb4f828582774b42a29f79e188a3e84228957b
n0aHVN <superherodung123@gmail.com>
2025-08-31 09:09:06 +0700

    Update PaymentEventInterface.ts

--- common/events/PaymentEventInterface.ts
+++ common/events/PaymentEventInterface.ts
@@ -1,25 +1,11 @@
-import { Types } from "mongoose";
 import { SubjectsEnum } from "../enums/event-subject.enum";
-import { PaymentStatus } from "../enums/payment-status.enum";
-
 interface PaymentAttrs{
     _id: string;
     order_id: string;
-    status: PaymentStatus;
-    version: string;
+    user_id: string;
 }
 
 export interface PaymentCreatedEventInterface{
     subject: SubjectsEnum.PaymentCreated;
-    data: PaymentAttrs;
-}
-
-export interface PaymentUpdatedEventInterface{
-    subject: SubjectsEnum.PaymentUpdated;
-    data: PaymentAttrs;
-}
-
-export interface PaymentExpiredEventInterface{
-    subject: SubjectsEnum.PaymentExpired;
     data: PaymentAttrs;
 }
