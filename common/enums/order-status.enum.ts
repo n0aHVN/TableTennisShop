9b55ad986226abc2d4c23e5439d2ecb3c04139ae
n0aHVN <superherodung123@gmail.com>
2025-08-31 09:09:01 +0700

    Update order-status.enum.ts

--- common/enums/order-status.enum.ts
+++ common/enums/order-status.enum.ts
@@ -1,9 +1,6 @@
 export enum OrderStatusEnum {
     PENDING = 'pending',         // Order placed but not yet processed
-    CONFIRMED = 'confirmed',
-    DELIVERING = 'delivering',   // Order is being shipped
     FINISHED = 'finished',       // Order completed/delivered
-    CANCELLED = 'cancelled',      // Order canceled by user or system
-    RETURNED = 'returned',
-    FAILED = 'failed'
+    EXPIRED = 'expired',      // Order expired
+    CANCELLED = 'cancelled'      // Order cancelled by user or system
 }
